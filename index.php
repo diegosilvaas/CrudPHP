103949f4acd3f095520e7e76f3507bdf7173b143
diegosilvaas <diegomecanica2014@gmail.com>
2023-05-15 20:23:48 -0400

    incluindo mostrar na tela

--- index.php
+++ index.php
@@ -5,16 +5,30 @@ $pdo = new PDO('mysql:host=localhost;dbname=crud', 'root', '');
 //insert  
 if (isset($_POST['nome'])) {
     $sql = $pdo->prepare("INSERT INTO clientes VALUES (null,?,?)");
-    $sql->execute(array($_POST['nome'], $_POST['email']));
+    $sql->execute(array($_POST['nome'], $_POST['telefone'], $_POST['email']));
     echo 'inserido com sucesso!';
 }
 ?>
 
 <form method="post">
     <input type="text" name="nome">
+    <input type="text" name="telefone">
     <input type="text" name="email">
     <input type="submit" name="Enviar">
-
-
-
 </form>
+
+// Mostrar na tela
+
+<?php
+    $sql = $pdo->prepare("SELECT * FROM clientes");
+    $sql->execute();
+
+    $fetchClientes = $sql->fetchAll();
+
+    foreach ($fetchClientes as $key => $value) {
+        echo $value['nome']. ' | '.$value['telefone']. ' | '.$value['email'];
+        echo '<hr>';
+    }
+    
+
+?>
