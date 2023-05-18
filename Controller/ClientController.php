a738f0f1c7be16c8c86a9812d42419e8ca15861d
diegosilvaas <diegomecanica2014@gmail.com>
2023-05-17 21:03:17 -0400

    resolvendo bug

--- Controller/ClientController.php
+++ Controller/ClientController.php
@@ -2,18 +2,19 @@
 
 include_once 'C:\xampp\htdocs\crudPHP\Model\Conection.php';
 
+include_once 'C:\xampp\htdocs\crudPHP\Model\Client.php';
+
 Class ClientController 
 {
     private $dados;
     private $clientRepository;
-}
 
     public function __construct()
     { 
- 
-    $this->pegaDadosFormulario();
-    $this->clienteRepository = new Cliente();
+        // $this->pegaDadosFormulario(); comentar esse mestodo a principio
+        $this->clientRepository = new Client(); 
     }
+}
 
 
 
